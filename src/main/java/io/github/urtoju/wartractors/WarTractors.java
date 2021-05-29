9970084833ae3b4cb6cf3ab5c8acd7ab7f32ce37
URTOJU <guglmonstar@gmail.com>
2021-05-29 15:32:02 +0200

    Initial

---
+++ src/main/java/io/github/urtoju/wartractors/WarTractors.java
@@ -0,0 +1,9 @@
+package io.github.urtoju.wartractors;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WarTractors implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/urtoju/wartractors/client/WarTractorsClient.java
@@ -0,0 +1,11 @@
+package io.github.urtoju.wartractors.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+
+@Environment(net.fabricmc.api.EnvType.CLIENT)
+public class WarTractorsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
