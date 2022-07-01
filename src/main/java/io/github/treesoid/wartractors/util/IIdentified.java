d579549de09fb10bd05db47b7cef05403ce0dc1e
Treesoid <70215358+Treesoid@users.noreply.github.com>
2022-07-01 08:45:58 +0200

    Remove everything
    Took 1 minute

--- src/main/java/io/github/treesoid/wartractors/util/IIdentified.java
+++
@@ -1,7 +0,0 @@
-package io.github.treesoid.wartractors.util;
-
-import net.minecraft.util.Identifier;
-
-public interface IIdentified {
-    Identifier getIdentifier();
-}
--- src/main/java/io/github/treesoid/wartractors/util/tractortypes/ITractorWeaponType.java
+++
@@ -1,6 +0,0 @@
-package io.github.treesoid.wartractors.util.tractortypes;
-
-import io.github.treesoid.wartractors.util.IIdentified;
-
-public interface ITractorWeaponType extends IIdentified {
-}
